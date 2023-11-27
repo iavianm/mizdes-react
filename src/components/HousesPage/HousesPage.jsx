58a7c3fd55543fc6fe3bed1729d024a98d078a6d
Mihail Iavian <85059359+iavianm@users.noreply.github.com>
2023-11-27 14:22:40 +0200

    PopupTour (#1)

--- src/components/HousesPage/HousesPage.jsx
+++ src/components/HousesPage/HousesPage.jsx
@@ -3,10 +3,20 @@ import Houses from "../Houses/Houses.jsx";
 import housesContent from "../../content/housesContent.json";
 import TerraceBlock from "../TerraceBlock/TerraceBlock.jsx";
 import heroContent from "../../content/heroContent.json";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import HousesPopup from "../HousesPopup/HousesPopup.jsx";
 // import TourPopup from "@/app/components/TourPopup/TourPopup";
 
 const HousesPage = ({ handleTogglePopup }) => {
+  const [openPopup, setOpenPopup] = useState(false);
+  const [selectedHouse, setSelectedHouse] = useState(null);
+
+  function handleTogglePopupTour(house) {
+    setSelectedHouse(house);
+    setOpenPopup(!openPopup);
+  }
+
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -20,17 +30,24 @@ const HousesPage = ({ handleTogglePopup }) => {
       <Houses
         house={housesContent.riviera}
         handleTogglePopup={handleTogglePopup}
+        handleTogglePopupTour={handleTogglePopupTour}
       />
       <Houses
         house={housesContent.grandis}
         handleTogglePopup={handleTogglePopup}
+        handleTogglePopupTour={handleTogglePopupTour}
       />
       <Houses
         house={housesContent.highgarden}
         handleTogglePopup={handleTogglePopup}
+        handleTogglePopupTour={handleTogglePopupTour}
       />
       <TerraceBlock />
-      {/*<TourPopup />*/}
+      <HousesPopup 
+      selectedHouse={selectedHouse}
+      isVisiblie={openPopup}
+      onClose={handleTogglePopupTour}
+      />
     </section>
   );
 };
