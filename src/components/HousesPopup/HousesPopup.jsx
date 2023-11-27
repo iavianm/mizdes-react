abfbce91f79541c6f663efc04a8f76d3aeeb8f3c
Mihail Iavian <85059359+iavianm@users.noreply.github.com>
2023-11-27 15:41:32 +0200

    Popup fix (#2)

--- src/components/HousesPopup/HousesPopup.jsx
+++ src/components/HousesPopup/HousesPopup.jsx
@@ -1,14 +1,10 @@
 import close from "../../../public/images/close-icon.svg";
-import housesPopupContent from '../../content/housesPopupContent.json';
-import './HousesPopup.css';
+import housesPopupContent from "../../content/housesPopupContent.json";
+import "./HousesPopup.css";
 
-export default function HousesPopup({
-  isVisiblie,
-  selectedHouse,
-  onClose
-}) 
-{
-  const { scheme, option, items, texts, quantity } = housesPopupContent.riviera;  
+export default function HousesPopup({ isVisiblie, selectedHouse, onClose }) {
+  const { scheme, option, items, texts, quantity } =
+    housesPopupContent.highgarden;
 
   return (
     <div className={`popup-overlay ${isVisiblie ? "popup-open" : ""}`}>
@@ -19,13 +15,23 @@ export default function HousesPopup({
         <div className="popup-container">
           <div className="popup-schemes">
             {scheme.map((img, index) => (
-              <img key={index} className="popup-scheme" src={img} alt={`Scheme ${index + 1}`} />
+              <img
+                key={index}
+                className="popup-scheme"
+                src={img}
+                alt={`Scheme ${index + 1}`}
+              />
             ))}
           </div>
           <div className="popup-content">
             <div className="popup-options">
               {option.map((opt, index) => (
-                <img key={index} className="popup-option" src={opt} alt={`Option ${index + 1}`} />
+                <img
+                  key={index}
+                  className="popup-option"
+                  src={opt}
+                  alt={`Option ${index + 1}`}
+                />
               ))}
             </div>
             <div className="popup-items">
