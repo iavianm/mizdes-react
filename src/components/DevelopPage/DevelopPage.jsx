4015431f038bad5c4a8381e62ff8ffb343a67ff5
Анастасия Маевская <Quri.mau@gmail.com>
2023-12-02 12:04:55 +0300

    relax block add

--- src/components/DevelopPage/DevelopPage.jsx
+++ src/components/DevelopPage/DevelopPage.jsx
@@ -1,4 +1,5 @@
 import Hero from "../Hero/Hero";
+import LeisureBlock from '../LeisureBlock/LeisureBlock';
 import "./DevelopPage.css";
 import heroContent from "../../content/heroContent.json";
 import { restaurant } from "../../content/sliderRestaurantImages.json";
@@ -56,6 +57,7 @@ export default function DevelopPage() {
             ))}
           </Slider>
         </div>
+        <LeisureBlock />
       </section>
     </>
   );
