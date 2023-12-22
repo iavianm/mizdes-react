2fa05d08f5c4bcc57d4ee51ee32edf5ab28e76e6
iavianm <iavianm@mail.ru>
2023-12-22 12:32:09 +0200

    fix price

--- src/components/VillaCard/VillaCard.jsx
+++ src/components/VillaCard/VillaCard.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import "./VillaCard.css";
 
-const VillaCard = ({ name, area, rooms, price, description, imageSrc }) => {
+const VillaCard = ({
+  name,
+  area,
+  rooms,
+  price,
+  nyPrice,
+  description,
+  imageSrc,
+}) => {
   return (
     <div className="card">
       <div className="imageWrapper">
@@ -12,15 +20,17 @@ const VillaCard = ({ name, area, rooms, price, description, imageSrc }) => {
         />
       </div>
       <div className="info">
-        <div className="about">
-          <h3 className="title">{name}</h3>
-          <div className="details">
-            <span className="area">{area}</span>
-            <span className="rooms">{rooms}</span>
-            <span>Цена: {price}</span>
+        <div className={"about-container"}>
+          <div className="about">
+            <h3 className="title">{name}</h3>
+            <div className="details">
+              <span className="area">{area}</span>
+              <span className="rooms">{rooms}</span>
+              <span>Цена: {price}</span>
+            </div>
           </div>
+          <div className={"card-price-new-year"}>{nyPrice}</div>
         </div>
-
         <p className="description">{description}</p>
       </div>
     </div>
